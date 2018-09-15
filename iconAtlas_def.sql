0b76c80bd5429c58acd7bf09bef32618be5fbd37
Arage <gernigirgarh@yopmail.com>
2018-09-15 18:33:51 +0200

    base files (to change)
    Extract from a larger project

---
+++ iconAtlas_def.sql
@@ -0,0 +1,21 @@
+-- ICONS ATLAS
+
+INSERT INTO IconTextureAtlases (Atlas, IconSize, Filename, IconsPerRow, IconsPerColumn) VALUES
+('VP_BUILDING_PACK1_ATLAS',256,'buildingPack_1_256.dds',6,2),
+('VP_BUILDING_PACK1_ATLAS',128,'buildingPack_1_128.dds',6,2),
+('VP_BUILDING_PACK1_ATLAS',80,'buildingPack_1_080.dds',6,2),
+('VP_BUILDING_PACK1_ATLAS',64,'buildingPack_1_064.dds',6,2),
+('VP_BUILDING_PACK1_ATLAS',45,'buildingPack_1_045.dds',6,2);
+
+-- CASINO 11
+-- GMO 8
+-- GOBEKLI 7
+-- METEORA 4
+-- PALIO DI SIENNA 3
+-- POLAR_STATION 9
+-- PRC 1
+-- TEMPLE_HEAVEN 2
+-- CLOCK_TOWER 10
+-- GREEKOLIVES RES 5
+
+
